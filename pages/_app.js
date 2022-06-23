dae301a547776c7c86b5d47f9f5e697d19a7ba40
mehmetelbeyli79 <mehmetelbeyli88@gmail.com>
2022-06-23 09:52:30 +0300

    Initializing

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Header from "./header";
+import React from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+
+        <Header/>
+        <Component {...pageProps} />
+      </>
+
+  )
 }
 
 export default MyApp
